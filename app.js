27d1c75e33e33dfcb00a992e1b72a17c1893bd8b
crontreras1 <crontreras1@gmail.com>
2025-08-18 14:14:32 -0500

    class 3. nvm version controling

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express(); 
 
-const PORT = 3000; 
+const PORT = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
-  res.send('Holi, mundillo ;)');
+  res.send(`
+    <h1>Backend con expressJs</h1>
+    <p>Este es el primer backend que estoy creando 😊</p>
+    <p>Corre en el puerto: ${ PORT }</p>
+    <p>Funciona!!</p>
+  `);
 });
 
 app.listen(PORT, () => {
