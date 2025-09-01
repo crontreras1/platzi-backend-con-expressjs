544446743e6d354550b1634763c2e6325c9c6d80
crontreras1 <crontreras1@gmail.com>
2025-09-01 18:38:23 -0500

    class 6. Postman for APi testing

--- app.js
+++ app.js
@@ -35,6 +35,32 @@ app.get('/search', (req, res) => {
   `);
 });
 
+app.post('/form', (req, res) => {
+  const name = req.body.name || 'Sin nombre'; 
+  const email = req.body.email || 'No proporcionado';
+
+  res.json({
+    message: 'Datos recibidos',
+    data: {
+      name,
+      email
+    }
+  });
+});
+
+app.post('/api/data', (req, res) => {
+  const data = req.body;
+
+  if(!data || Object.keys(data).length === 0) {
+    return res.status(400).json({ error: 'No se recibiron datos' });
+  };
+
+  res.status(201).json({
+    message: 'Datos JSON recibidos',
+    data
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor: http://localhost:${ PORT }`); 
 }); 
