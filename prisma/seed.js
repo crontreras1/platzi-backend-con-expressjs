e7ac6fe365944469ed7fb786cae68432e6aafd6f
crontreras1 <crontreras1@gmail.com>
2025-12-17 13:53:51 -0500

    class 17. creation and user generation  with prisma and nodejs

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,21 +2,21 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  const demoUsers = [
-    { name: 'Cristian', email: 'cristian@example.com' },
-    { name: 'Camilo', email: 'camilo@example.com' },
-    { name: 'Jorge', email: 'jorge@example.com' }
-  ];
+  // const demoUsers = [
+  //   { name: 'Cristian', email: 'cristian@example.com' },
+  //   { name: 'Camilo', email: 'camilo@example.com' },
+  //   { name: 'Jorge', email: 'jorge@example.com' }
+  // ];
 
-  for (const user of demoUsers) {
-    await prisma.user.create({
-      data: user
-    });
-  }
+  // for (const user of demoUsers) {
+  //   await prisma.user.create({
+  //     data: user
+  //   });
+  // }
 
-  console.log('Usuarios de demostraciÃ³n creados con Ã©xito');
+  // console.log('Usuarios de demostración creados con éxito');
 
-  // await prisma.user.deleteMany();
+  await prisma.user.deleteMany();
 }
 
 main()
